6aec7f4085125e6af96b3683f76d1afb39cd1a3e
LeonardoM011 <Leonardo.leo.201@gmail.com>
2017-10-08 14:26:47 +0200

    Basics of GL Buffer
    Signed-off-by: LeonardoM011 <Leonardo.leo.201@gmail.com>

--- KobeGE/src/Main.cpp
+++ KobeGE/src/Main.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,19 +21,33 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK) {
+		//std::cout << "Error 0: Glew init failed!\n";
+	}
+
+	std::cout << "OPENGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+	float positions[] = {
+		-0.5f, -0.5f,
+		0.0f,   0.5f,
+		0.5f,  -0.5f
+	};
+	GLuint vboid;
+	glGenBuffers(1, &vboid);
+	glBindBuffer(GL_ARRAY_BUFFER, vboid);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		// LEGACY OPENGL (DON'T USE THIS) --
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-		//----------------------------------
+		
+		//-----------------------------------------------
+		int count = sizeof(positions) / sizeof(float) / 2;
+		glDrawArrays(GL_TRIANGLES, 0, count);
+		//-----------------------------------------------
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
